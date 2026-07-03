c26c0d25bb8c6c42116b30076cf860e8ec41101a
asm-saim <saim777m@gmail.com>
2026-07-03 13:10:30 +0600

    fixed warning for only class insted of calssNae in jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,13 +4,13 @@ import coin from '../../assets/dollar.png'
 
 const Navbar = () => {
     return (
-        <div class="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
-            <div class="flex-1">
-                <a class="">
+        <div className="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
+            <div className="flex-1">
+                <a>
                     <img className='w-15 h-15' src={navImg} alt="" />
                 </a>
             </div>
-            <div class="flex items-center gap-3 ">
+            <div className="flex items-center gap-3 ">
                 <span>600000000 Coin</span>
                 <span><img className='w-7 h-7' src={coin} alt="" /></span>
             </div>
