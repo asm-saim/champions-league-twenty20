81328738226a97b2e3e7f2fcb885dfd11b13589c
asm-saim <saim777m@gmail.com>
2026-07-04 00:22:50 +0600

    toggling feature added with useEffect hook and conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,34 @@ import './App.css'
 import AvailablePlayers from './components/AvailablePlayers/AvailablePlayers'
 import Navbar from './components/Navbar/Navbar'
 import SelectedPlayers from './components/SelectedPlayers/SelectedPlayers'
+import { useState } from 'react'
 
 // Fetch data from the API
 const fetchPlayers = fetch('/players.json').then(res => res.json())
 
 function App() {
 
+  //state declaration for toggling:
+  const [toggle, setToggle] = useState(true)
   return (
     <>
-      <div className='max-w-7xl mx-auto'>
+      <div className='mx-auto'>
         <Navbar></Navbar>
-        <div className='flex justify-between items-center border border-amber-700'>
-          <span>Available Players</span>
-          <div className='flex gap-4 '>
-            <button className='btn'>Available</button>
-            <button className='btn'>Selected(0)</button>
+        <div className='max-w-7xl mx-auto'>
+          <div className='flex justify-between items-center my-5'>
+            <span className='font-bold text-xl'>Available Players</span>
+            <div className='flex text-base'>
+              <button onClick={() => setToggle(true)} className={`btn rounded-l-lg border-none font-bold text-base ${toggle === true ? "bg-orange-500 text-black" :"'' text-white"}`}>Available</button>
+              <button onClick={() => setToggle(false)} className={`btn rounded-r-lg border-none font-bold text-base ${toggle === false?"bg-orange-500 text-black": 
+              "'' text-white"}`}>Selected(0)</button>
+            </div>
           </div>
+          {
+            toggle === true ?
+              <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}><AvailablePlayers fetchPlayers={fetchPlayers}></AvailablePlayers></Suspense> : <SelectedPlayers></SelectedPlayers>
+          }
+
         </div>
-        <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
-          <AvailablePlayers fetchPlayers={fetchPlayers}></AvailablePlayers>
-        </Suspense>
-        <SelectedPlayers></SelectedPlayers>
       </div>
 
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,14 +4,14 @@ import coin from '../../assets/dollar.png'
 
 const Navbar = () => {
     return (
-        <div className="navbar bg-base-100 shadow-sm max-w-7xl mx-auto">
+        <div className="navbar shadow-sm px-9 mx-auto bg-gray-700">
             <div className="navbar-start">
                 <a>
                     <img className='w-15 h-15' src={navImg} alt="" />
                 </a>
             </div>
             <div className="navbar-center">
-                <h2 className='font-bold text-3xl text-orange-500'>Big Bash League — Build Your Dream 11</h2>
+                <h2 className='font-extrabold text-3xl text-white'>Big Bash League — Build Your Dream 11</h2>
             </div>
             <div className="navbar-end flex items-center gap-3 ">
                 <span>600000000 Coin</span>
