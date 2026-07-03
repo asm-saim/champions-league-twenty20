6797bd6deba060af6ad0d23e67982e8e196e3c80
asm-saim <saim777m@gmail.com>
2026-07-03 22:26:15 +0600

    added all players img url and implemented some css on the SPA

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -3,7 +3,7 @@ import flag from '../../assets/flag.png'
 
 const Player = ({ player }) => {
     return (
-        <div class="card bg-base-100 shadow-sm p-5">
+        <div class="card bg-base-100 shadow-sm p-5 bg-white">
             <figure className="rounded-xl">
                 <img className='h-55 w-full object-cover'
                     src={player.playerImage} />
@@ -11,26 +11,26 @@ const Player = ({ player }) => {
             <div class="">
                 <div className="flex items-center gap-2 my-5">
                     <img className="h-7 w-7" src={profilePc} alt="" />
-                    <h2 class="card-title font-bold">{player.playerName}</h2>
+                    <h2 class="card-title font-bold text-black">{player.playerName}</h2>
                 </div>
-                <div className="flex justify-between items-center mb-4 pb-3 border-b border-gray-300">
+                <div className="flex justify-between items-center mb-4 pb-3 border-b border-gray-500">
                     <div className="flex items-center gap-2">
                         <img className="w-7 h-7" src={flag} alt="" />
-                        <span className="font-semibold text-gray-500">{player.origin}</span>
+                        <span className="font-semibold text-gray-700">{player.origin}</span>
                     </div>
-                    <button className="btn btn-sm text-gray-500">{player.role}</button>
+                    <p className="py-1 px-2 rounded-lg text-sm bg-orange-100 border-none text-black">{player.role}</p>
                 </div>
                 <div className="flex justify-between items-center mb-2">
-                    <span className="font-semibold">Rating</span>
+                    <span className="font-semibold text-black">Rating</span>
                     <span className="font-semibold text-gray-700">{player.rating}</span>
                 </div>
                 <div className="flex justify-between items-center mb-2">
-                    <span className="font-semibold">Batting Style</span>
-                    <span className="font-semibold text-gray-500">{player.battingStyle}</span>
+                    <span className="font-semibold text-black">Batting Style</span>
+                    <span className="font-semibold text-gray-700">{player.battingStyle}</span>
                 </div>
                 <div className="flex justify-between items-center">
-                    <span className="font-semibold">Price: $<span>{player.price}</span></span>
-                    <button className="btn btn-sm text-gray-500">Choose Player</button>
+                    <span className="font-semibold text-black">Price: $<span>{player.price}</span></span>
+                    <button className="btn font-bold bg-orange-500 border-none text-black">Choose Player</button>
                 </div>
 
             </div>
