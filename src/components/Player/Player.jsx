5184855627ab193a2c05a3300969397a814a5e09
asm-saim <saim777m@gmail.com>
2026-07-04 19:38:04 +0600

    ui style changed

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -20,12 +20,11 @@ const Player = ({ player, setAvailableBalance, balance, purchasedPlayer, setPurc
         setSelected(true)
         setAvailableBalance(balance - playerPrice)
         setPurchasedPlayer([...purchasedPlayer, player])
-        
-        toast.success("Player Selected!", { theme: 'dark' })
+        toast.success("Player Selected!", {theme:'dark'})
     }
 
     return (
-        <div className="card bg-base-100 shadow-sm p-5 bg-white">
+        <div className="card bg-base-100 shadow-sm p-5 bg-gray-700">
             <figure className="rounded-xl">
                 <img className='h-55 w-full object-cover'
                     src={player.playerImage} />
@@ -33,28 +32,28 @@ const Player = ({ player, setAvailableBalance, balance, purchasedPlayer, setPurc
             <div>
                 <div className="flex items-center gap-2 my-5">
                     <img className="h-7 w-7" src={profilePc} alt="" />
-                    <h2 className="card-title font-bold text-black">{player.playerName}</h2>
+                    <h2 className="card-title font-bold">{player.playerName}</h2>
                 </div>
                 <div className="flex justify-between items-center mb-3 pb-3 border-b border-gray-500">
                     <div className="flex items-center gap-2">
                         <img className="w-7 h-7" src={flag} alt="" />
-                        <span className="font-semibold text-gray-700">{player.origin}</span>
+                        <span className="font-semibold text-gray-200">{player.origin}</span>
                     </div>
-                    <p className="py-1 px-2 rounded-lg text-sm bg-orange-100 border-none text-black">{player.role}</p>
+                    <p className="py-1 px-2 rounded-lg text-sm font-semibold bg-orange-100 border-none text-black">{player.role}</p>
                 </div>
                 <div className="flex justify-between items-center mb-1">
-                    <span className="font-semibold text-black">Rating</span>
-                    <span className="font-semibold text-gray-700">{player.rating}</span>
+                    <span className="font-semibold">Rating</span>
+                    <span className="font-semibold">{player.rating}</span>
                 </div>
                 <div className="flex justify-between items-center mb-2">
-                    <span className="font-semibold text-black">Batting Style</span>
-                    <span className="font-semibold text-gray-700">{player.battingStyle}</span>
+                    <span className="font-semibold">Batting Style</span>
+                    <span className="font-semibold">{player.battingStyle}</span>
                 </div>
                 <div className="flex justify-between items-center">
-                    <span className="font-semibold text-black">Price: $<span>{player.price}</span></span>
+                    <span className="font-semibold">Price: $<span>{player.price}</span></span>
                     <button disabled={isSelected} onClick={() => handleSelected(player)}
-                        className={` border-none text-black btn font-bold 
-                    ${isSelected ? "bg-orange-200" : "bg-orange-500"}`}>
+                        className={` border-none text-black btn font-bold text-base 
+                    ${isSelected ? "bg-orange-100" : "bg-orange-500"}`}>
                         {isSelected === true ? "Selected" : "Choose Player"}</button>
                 </div>
 
